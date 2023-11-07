ae77963b6c54ca7b0da2b5c486caad8fe90a5da8
Samuel <samuel.okafor@decagon.dev>
2023-11-07 18:32:13 +0100

    update

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationRepository.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationRepository.java
@@ -1,7 +1,15 @@
 package com.skyapi.weatherforecast.location;
 
 import com.skyapi.weatherforecast.common.Location;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends CrudRepository<Location, String> {
+    @Query("SELECT l from Location l where l.trashed= false")
+    List<Location> findUntrashed();
+
+    @Query("SELECT l from Location l where l.trashed= false AND l.code=?1")
+    Location findByCode(String code);
 }
