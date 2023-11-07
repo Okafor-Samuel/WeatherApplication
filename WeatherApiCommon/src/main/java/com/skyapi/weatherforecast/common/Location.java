9e049a3a6983d7435c7013dfb64f424ee4e660a3
Samuel <samuel.okafor@decagon.dev>
2023-11-07 14:13:48 +0100

    update

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -1,6 +1,10 @@
 package com.skyapi.weatherforecast.common;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +18,26 @@ import javax.annotation.processing.Generated;
 @Table(name = "locations")
 public class Location {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     @Column(length = 12, nullable = false, unique = true)
+    @NotBlank
     private String code;
     @Column(length = 128, nullable = false)
+    @JsonProperty("city_name")
+    @NotBlank
     private String cityName;
     @Column(length = 128)
+    @JsonProperty("region_name")
+    @NotNull
     private String regionName;
     @Column(length = 64, nullable = false)
+    @JsonProperty("country_name")
+    @NotBlank
     private String countryName;
     @Column(length = 4, nullable = false)
+    @JsonProperty("country_code")
+    @NotBlank
     private String countryCode;
     private boolean enabled;
+    @JsonIgnore
     private boolean trashed;
 }
