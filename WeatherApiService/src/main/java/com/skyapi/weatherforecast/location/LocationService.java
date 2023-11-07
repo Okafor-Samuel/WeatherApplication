233396cea8df189d0428edd67f765c516d9690e4
Samuel <samuel.okafor@decagon.dev>
2023-11-07 16:52:05 +0100

    update

---
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationService.java
@@ -0,0 +1,21 @@
+package com.skyapi.weatherforecast.location;
+
+import com.skyapi.weatherforecast.common.Location;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+    private final LocationRepository locationRepository;
+
+    public Location add(Location location){
+        return locationRepository.save(location);
+    }
+
+    public List<Location> list(){
+        return locationRepository.findUntrashed();
+    }
+}
