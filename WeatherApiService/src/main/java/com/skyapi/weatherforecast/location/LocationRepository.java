306182e1a69fcafcd021547280e207d851bad0b3
Samuel <samuel.okafor@decagon.dev>
2023-11-19 08:03:30 +0100

    delete location

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationRepository.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/LocationRepository.java
@@ -1,6 +1,7 @@
 package com.skyapi.weatherforecast.location;
 
 import com.skyapi.weatherforecast.common.Location;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -12,4 +13,8 @@ public interface LocationRepository extends CrudRepository<Location, String> {
 
     @Query("SELECT l from Location l where l.trashed= false AND l.code=?1")
     Location findByCode(String code);
+
+    @Modifying
+    @Query("UPDATE Location SET trashed = true WHERE code=?1")
+    public void trashedByCode(String code);
 }
