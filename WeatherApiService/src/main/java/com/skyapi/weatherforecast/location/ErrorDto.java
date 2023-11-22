44d8b90874f775c999f668103cddf7c21b0550c9
Samuel <samuel.okafor@decagon.dev>
2023-11-22 18:08:53 +0100

    exception handling

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/ErrorDto.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/location/ErrorDto.java
@@ -3,8 +3,11 @@ package com.skyapi.weatherforecast.location;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,5 +16,9 @@ public class ErrorDto {
     private Date timeStamp;
     private int status;
     private String path;
-    private String error;
+    private  List<String> errors = new ArrayList<>();
+
+    public void addError(String message){
+        this.errors.add(message);
+    }
 }
