ad68e77679c28d7ccc02613109d8ad569a87d298
Samuel <samuel.okafor@decagon.dev>
2023-11-28 00:27:16 +0100

    validating requestBody

--- WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.annotation.processing.Generated;
 
@@ -20,23 +21,34 @@ public class Location {
     @Id
     @Column(length = 12, nullable = false, unique = true)
     @NotNull(message = "Location code cannot be null")
+    @Length(min = 3, max = 12, message = "Location code must have 3 to 12 characters")
     private String code;
+
     @Column(length = 128, nullable = false)
     @JsonProperty("city_name")
-    @NotBlank(message = "City name cannot be blank")
+    @NotNull(message = "City name cannot be null")
+    @Length(min = 3, max = 120, message = "City name must have 3 to 120 characters")
     private String cityName;
+
     @Column(length = 128)
     @JsonProperty("region_name")
+    @Length(min = 3, max = 120, message = "Region name must have 3 to 120 characters")
     private String regionName;
+
     @Column(length = 64, nullable = false)
     @JsonProperty("country_name")
-    @NotBlank(message = "Country name cannot be blank")
+    @NotNull(message = "Country name cannot be null")
+    @Length(min = 3, max = 64, message = "Country name must have 3 to 64 characters")
     private String countryName;
+
     @Column(length = 4, nullable = false)
     @JsonProperty("country_code")
-    @NotBlank(message = "Country code cannot be blank")
+    @NotNull (message = "Country code cannot be null")
+    @Length(min = 2, max = 2, message = "Country code must have 2 characters")
     private String countryCode;
+
     private boolean enabled;
+
     @JsonIgnore
     private boolean trashed;
 }
