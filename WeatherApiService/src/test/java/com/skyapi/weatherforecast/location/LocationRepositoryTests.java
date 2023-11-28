d17de7943262839f25eb7b9116161b3fe53bd371
Samuel <samuel.okafor@decagon.dev>
2023-11-28 02:01:00 +0100

    real time weather API

--- WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationRepositoryTests.java
+++ WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationRepositoryTests.java
@@ -1,12 +1,14 @@
 package com.skyapi.weatherforecast.location;
 
 import com.skyapi.weatherforecast.common.Location;
+import com.skyapi.weatherforecast.common.RealtimeWeather;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -60,12 +62,34 @@ public class LocationRepositoryTests {
 
     @Test
     public void testTrashedSuccess() {
-        String code = "b";
+        String code = "c";
         locationRepository.trashedByCode(code);
 
         Location location = locationRepository.findByCode(code);
-        assertThat(location).isNotNull();
-        assertThat(location.getCode()).isEqualTo(code);
+        assertThat(location).isNull();
+    }
+
+
+    @Test
+    public void testAddRealtimeWeatherData(){
+        String code = "b";
+
+        Location location = locationRepository.findByCode(code);
+        RealtimeWeather realtimeWeather = location.getRealtimeWeather();
+        if(realtimeWeather == null){
+            realtimeWeather = new RealtimeWeather();
+            realtimeWeather.setLocation(location);
+            location.setRealtimeWeather(realtimeWeather);
+        }
+        realtimeWeather.setTemperature(10);
+        realtimeWeather.setHumidity(60);
+        realtimeWeather.setPrecipitation(70);
+        realtimeWeather.setStatus("Sunny");
+        realtimeWeather.setWindSpeed(10);
+        realtimeWeather.setLastUpdated(new Date());
+
+        Location updatelocation = locationRepository.save(location);
+        assertThat(updatelocation.getRealtimeWeather().getLocationCode()).isEqualTo(code);
     }
 
 }
