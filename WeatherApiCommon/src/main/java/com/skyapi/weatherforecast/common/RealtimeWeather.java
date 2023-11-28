994784718b667719d199ff1430fdb99f99b44b1c
Samuel <samuel.okafor@decagon.dev>
2023-11-28 01:28:18 +0100

    real time weather API

---
+++ WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/RealtimeWeather.java
@@ -0,0 +1,39 @@
+package com.skyapi.weatherforecast.common;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Length;
+
+import java.util.Date;
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode
+@Table(name = "realtime_weather")
+
+public class RealtimeWeather {
+    @Id
+    @Column(name = "location_code")
+    private String locationCode;
+
+    private int temperature;
+
+    private int humidity;
+
+    private int precipitation;
+
+    private int windSpeed;
+
+    @Column(length = 50)
+    private String status;
+
+    private Date lastUpdated;
+
+    @OneToOne
+    @JoinColumn(name = "location_code")
+    @MapsId
+    private Location location;
+
+}
